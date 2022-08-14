793bd4cf585c359eda3055481d9bc1e8ed2eca9a
arjavsethi <75158301+arjavsethi@users.noreply.github.com>
2022-08-14 22:05:41 +0530

    basic next +firebase +bootstrap setup

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from '../components/NavBar';
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+   <NavBar/>
+  <Component {...pageProps} />
+  </>)
+}
+
+export default MyApp
